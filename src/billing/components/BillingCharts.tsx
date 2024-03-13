4d9ca0d2e9fa1dadc96fcc6a5a37b5f15426509d
guido morabito <guidomorabito161@hotmail.com>
2024-03-12 21:22:32 -0300

    total income from each year

--- src/billing/components/BillingCharts.tsx
+++ src/billing/components/BillingCharts.tsx
@@ -1,36 +1,48 @@
-import { Grid, Box, Typography } from '@mui/material'
-import { useContext, useEffect } from 'react'
+import { Grid, Box, Typography, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
 import { VictoryAxis, VictoryBar, VictoryChart, VictoryPie } from 'victory'
 import { DataContext } from '../../context/DataBillingContext/DataContext'
+import { Bill } from '../../context/DataBillingContext/DataProvider'
 
 const BillingCharts = () => {
     const { bills, getData, getPaymentByYear, payments } = useContext(DataContext)
+    const [selectedYears, setSelectedYears] = useState<string[]>([])
     const totalUnpaid = bills.filter((bill) => bill.paid === 'No').length
     const totalPaid = bills.filter((bill) => bill.paid === 'Yes').length
+    let activeYears: string[] = []
 
-    const splitByMonth = () => {
-        let months: string[] = []
+    const splitByYear = () => {
+        let uniqueYears: Set<string> = new Set();
+
         bills.forEach((bill) => {
-            const month = bill.date.split('/')[1]
-            months.push(month)
-        })
-        console.log(months);
-        return months
-    }
+            const year = bill.date.split('/')[2];
+            uniqueYears.add(year);
+        });
+
+        const yearsArray = Array.from(uniqueYears);
+        setSelectedYears(yearsArray);
+        activeYears = yearsArray
+    };
 
 
 
     useEffect(() => {
-        getPaymentByYear(2023)
-        console.log(payments);
-
         getData()
+        getPaymentByYear(2023)
+        console.log(activeYears);
+
+        // setTimeout(() => {
+        splitByYear()
+        // }, 1000);
+
     }, [])
 
 
+
+
     return (
-        <Grid p={5} display={'flex'} flexDirection={'column'} alignItems={'center'}>
-            <Box p={5} width={'70%'} height={300} display={'flex'} justifyContent={'space-around'} alignItems={'center'}
+        <Grid mt={10} p={5} display={'flex'} flexDirection={'column'} alignItems={'center'}>
+            <Box p={5} height={300} width={'70%'} display={'flex'} justifyContent={'space-around'} alignItems={'center'}
                 boxShadow={7}
                 sx={{ borderRadius: 5 }}>
                 <Typography fontWeight={600} width={350} variant='h4' color='black'>Paid / Unpaid Bills</Typography>
@@ -49,54 +61,73 @@ const BillingCharts = () => {
                     />
                 </Box>
             </Box>
-            <Box mt={10} p={5} width={'70%'} height={300} display={'flex'} justifyContent={'space-around'} alignItems={'center'}
+            <Box mt={10} p={5} height={500} width={'70%'} display={'flex'} flexDirection={'column'} justifyContent={'space-around'} alignItems={'center'}
                 boxShadow={7}
                 sx={{ borderRadius: 5 }}>
-                <Typography fontWeight={600} width={350} variant='h4' color='black'>Month History</Typography>
-                <Box height={300}>
+                <Box display={'flex'} justifyContent={'space-around'} width={'80%'}>
+                    <Typography textAlign={'center'} fontWeight={600} maxWidth={450} variant='h4' color='black'>Total income each month</Typography>
+                    <FormControl>
+                        <InputLabel color='info' >Year</InputLabel>
+                        <Select
+                            size="small"
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            // value={first}
+                            label="Month"
+                            fullWidth
+                            color='info'
+                            // onChange={handleChange}
+                            sx={{
+                                backgroundColor: '#ffffff',
+                                '&:hover': {
+                                    borderColor: 'black', // Color fijo para el fondo al pasar el ratón
+                                },
+                                '& fieldset': {
+                                    borderColor: 'black', // Color fijo del borde
+                                },
+                                '& .MuiSelect-select': {
+                                    color: 'black', // Cambia el color del texto seleccionado
+                                },
+                                '& .MuiPaper-root': {
+                                    backgroundColor: 'black', // Cambia el color de fondo del menú desplegable
+                                },
+                            }}
+                        >
+                            {activeYears.map(year => 
+                                <MenuItem>{year}</MenuItem>
+                            )}
+    
+    {/* <MenuItem value={year}>{year}</MenuItem> */}
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box height={300} width={'100%'}>
                     <VictoryChart height={500} width={1500}
                         domainPadding={{ x: 50, y: [0, 20] }}
                         scale={{ x: "time" }}
                     >
                         <VictoryBar
-                            // dataComponent={
-                            //     <Bar events={{ onMouseOver: handleMouseOver }} />
-                            // }
                             labels={({ datum }) => `$${datum.y}`}
                             style={{
                                 data: { fill: "#0288d1", width: 50 },
                                 labels: { fontSize: '32px' }
                             }
                             }
-                            // data={[
-                            //     { x: 'January', y: 1 },
-                            //     { x: 'February', y: 2 },
-                            //     { x: 'March', y: 3 },
-                            //     { x: 'April', y: 4 },
-                            //     { x: 'May', y: 5 },
-                            //     { x: 'June', y: 6 },
-                            //     { x: 'July', y: 7 },
-                            //     { x: 'August', y: 8 },
-                            //     { x: 'September', y: 9 },
-                            //     { x: 'October', y: 10 },
-                            //     { x: 'November', y: 11 },
-                            //     { x: 'Dicember', y: 12 }
-                            // ]}
                             data={payments.map((payment) => ({ x: `${payment.month}/2024`, y: payment.total }))}
                         />
                         <VictoryAxis
                             style={{
-                                axisLabel: { fontSize: '25px', padding: 30 },
+                                axisLabel: { fontSize: '25px' },
                                 tickLabels: { fontSize: '25px' }
                             }}
                         />
 
                         {/* Configurar el eje Y */}
-                        <VictoryAxis    
+                        <VictoryAxis
                             dependentAxis={false}
                             style={{
                                 axisLabel: { fontSize: '25px' },
-                                tickLabels: { fontSize: '25px'}
+                                tickLabels: { fontSize: '25px' }
                             }}
                         />
                     </VictoryChart>
